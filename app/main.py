b2d15060ca8810bc331f235e5a9226fc6e27d53f
Hamza <hamzakat@protonmail.com>
2022-01-12 08:58:17 +0300

    remove unnecessary stuff

--- app/main.py
+++ app/main.py
@@ -1,9 +1,6 @@
 import time
-from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.params import Body
 from pydantic import BaseModel
-from random import randrange   
 import psycopg2  
 from psycopg2.extras import RealDictCursor
 
